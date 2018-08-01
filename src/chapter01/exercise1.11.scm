f63c25ad394da0cc8cb72e67725d99daee22af5f
Yakov Annikov <annikoff@ya.ru>
2018-08-01 09:27:22 +0300

    Format code

--- src/chapter01/exercise1.11.scm
+++ src/chapter01/exercise1.11.scm
@@ -1,15 +1,15 @@
 (define (f-rec n)
-  (if (< n 3)
-      n
-      (+
-        (f-rec (- n 1))
-        (f-rec (- n 2))
-        (f-rec (- n 3)))))
+ (if (< n 3)
+  n
+  (+
+   (f-rec (- n 1))
+   (f-rec (- n 2))
+   (f-rec (- n 3)))))
 
 (define (f-iter n)
-  (define (iter x y z counter)
-    (if (= counter 0)
-        z
-        (iter (+ x y z) x y (- counter 1))))
+ (define (iter x y z counter)
+  (if (= counter 0)
+   z
+   (iter (+ x y z) x y (- counter 1))))
 
-  (iter 2 1 0 n))
+ (iter 2 1 0 n))
